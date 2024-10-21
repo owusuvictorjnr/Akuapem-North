4fe093f4cd7e6438cf7aa9b0156c8acf84f1e414
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-21 14:08:47 +0000

    import personalities component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Personalities from "@/components/Personalities";
+
 export default function Home() {
   return (
     <div className="">
-      <div className="h-screen">home page</div>
+      <div className="h-screen">
+        <Personalities />
+      </div>
     </div>
   );
 }
