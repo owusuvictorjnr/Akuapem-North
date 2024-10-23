4612aeb9ca15fa86a54622b510cae8ba99fc4e16
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-23 13:28:16 +0000

    refactor: add view more button and link component to ful page

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ProjImage = [
@@ -37,7 +38,7 @@ const ProjImage = [
     image: "/proj/proj-6.jpg",
   },
 
-  { 
+  {
     name: "Road Construction in Abiriw",
     image: "/proj/proj-7.jpg",
   },
@@ -45,27 +46,37 @@ const ProjImage = [
 
 const Project = () => {
   return (
-    <div className="mt-36 min-h-[20rem] space-y-10 mx-auto ">
+    <div className="mt-36 min-h-[20rem] space-y-10">
       <div className="flex justify-center">
         <h1 className="font-bold text-2xl">Projects</h1>
       </div>
-      <div className="flex flex-col items-center gap-10 md:grid grid-cols-1 mx-auto md:grid-cols-4 lg:grid-cols-6">
+      <div className="flex flex-col items-center px-10 gap-10 md:grid grid-cols-2 mx-auto md:grid-cols-2 lg:grid-cols-4">
         {ProjImage.map((project, i) => (
-          <div key={i} className="bg-gray-200 rounded">
+          <div
+            key={i}
+            className="bg-gray-200 rounded w-[50vh] md:w-full shadow-md mx-auto cursor-pointer lg:hover:scale-95 duration-100 h-full lg:w-full"
+          >
             <div className="text-center">
-              <Image
-                src={project.image}
-                alt={project.name}
-                height={500}
-                width={500}
-                className="w-96"
-              />
-              <div className="">
-                <h2 className="py-2">{project.name}</h2>
-              </div>
+              <Link href="/projects">
+                <Image
+                  src={project.image}
+                  alt={project.name}
+                  height={500}
+                  width={500}
+                  className="h-52 w-full"
+                />
+                <div className="flex justify-center items-center mt-5 pb-5">
+                  <h2 className="py-2">{project.name}</h2>
+                </div>
+              </Link>
             </div>
           </div>
         ))}
+      </div>
+      <div className="flex justify-center items-center">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
+          View More
+        </button>
       </div>
     </div>
   );
