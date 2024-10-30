805f0a6a43e391b5d5a335298f558d33cfecc2e8
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-30 06:41:53 +0000

    news data

---
+++ data/newsData.ts
@@ -0,0 +1,27 @@
+// app/data/newsData.ts
+
+export const newsData = [
+  {
+    id: 1,
+    title: "New Constituency Hospital Launched",
+    description:
+      "The Akuapem North constituency launches a new hospital to improve healthcare services.",
+    date: "2024-10-20",
+    imageUrl: "/proj/sami-award.jpeg",
+  },
+  {
+    id: 2,
+    title: "Town Hall Meeting with Party Officials",
+    description: "Engage with key officials at our upcoming town hall meeting.",
+    date: "2024-10-15",
+    imageUrl: "/img/bawu.png",
+  },
+  {
+    id: 3,
+    title: "Education Fundraiser for Youth Development",
+    description:
+      "Join us in raising funds for the constituency's youth educational development.",
+    date: "2024-10-10",
+    imageUrl: "/img/camp-1.jpg",
+  },
+];
