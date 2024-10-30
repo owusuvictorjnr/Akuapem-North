ace8a3f401d7fa7cd98862979a80c58ba610dab2
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-30 06:28:07 +0000

    home page project

--- app/(frontend)/projects/page.tsx
+++ app/(frontend)/projects/page.tsx
@@ -3,61 +3,79 @@ import React from "react";
 
 const projImage = [
   {
-    name: "Water Supply Initiative in Akropong",
+    title: "Water Supply Initiative in Akropong",
+    description: "This is a water supply initiative in Akropong",
     image: "/proj/proj-8.jpg",
   },
 
   {
-    name: "Road Construction in Abiriw",
+    title: "Road Construction in Abiriw",
+    description: "This is a road construction project in Abiriw",
     image: "/proj/proj-1.jpg",
   },
 
   {
-    name: "Education Initiative in Larteh",
+    title: "Education Initiative in Larteh",
+    description: "This is an education initiative in Larteh",
     image: "/proj/proj-2.jpg",
   },
 
   {
-    name: "Agricultural Initiative in Adukrom",
+    title: "Agricultural Initiative in Adukrom",
+    description: "This is an agricultural initiative in Adukrom",
     image: "/proj/proj-3.jpg",
   },
 
   {
-    name: "Youth Empowerment in Mamfe",
+    title: "Youth Empowerment in Mamfe",
+    description: "This is a youth empowerment project in Mamfe",
     image: "/proj/proj-4.jpg",
   },
 
   {
-    name: "Water Supply Initiative in Akropong",
+    title: "Water Supply Initiative in Akropong",
+    description: "This is a water supply initiative in Akropong",
     image: "/proj/proj-5.png",
   },
 
   {
-    name: "Healthcare Initiative in Amanokrom",
+    title: "Healthcare Initiative in Amanokrom",
+    description: "This is a healthcare initiative in Amanokrom",
     image: "/proj/proj-6.jpg",
   },
 
   {
-    name: "Road Construction in Abiriw",
+    title: "Road Construction in Abiriw",
+    description: "This is a road construction project in Abiriw",
     image: "/proj/proj-7.jpg",
   },
 ];
 export default function ProjectPage() {
   return (
-    <div className="mt-36">
+    <div className="">
       <div className="">
-        <h1 className="">Projects</h1>
+        <h1 className="text-center text-2xl font-bold uppercase mb-4 bg-gray-200 py-2">
+          Projects
+        </h1>
       </div>
-      <div className="">
+      <div className="flex flex-col gap-10">
         {projImage.map((project, i) => (
-          <div key={i} className="">
-            <div className="">
+          <div
+            key={i}
+            className="bg-gray-200 min-h-[35rem] mx-auto container px- shadow-lg rounded-lg"
+          >
+            <div className="space-y-3">
               <Image
                 src={project.image}
-                alt={project.name}
-                height={200}
-                width={200}
+                alt={project.title}
+                height={500}
+                width={500}
+                className="w-full h-[30vh]"
               />
+              <div className="space-y-8">
+                <h1 className="text-center font-semibold">{project.title}</h1>
+                <p className="px-5">{project.description}</p>
+              </div>
             </div>
           </div>
         ))}
