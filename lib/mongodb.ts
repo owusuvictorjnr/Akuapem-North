56d4eaf3fe6ffe935685ade24fbad3adc0f81f1a
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-30 06:42:52 +0000

    mongodb setup

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,16 +1,29 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI || "";
+const MONGODB_URI = process.env.MONGODB_URI || '';
 
-
-if (!MONGO_URI) {
-  throw new Error("Please define the MONGO_URI environment variable inside .env.local");
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
+let cached = global.mongoose;
 
-export const connectDB = async () => {
-    if(mongoose.connections[0].readyState){
-        console.log("Already connected to database");
-        return;
-    }
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
