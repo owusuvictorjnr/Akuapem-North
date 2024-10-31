2d081a1b4e3aefd259c8f8ccfd3e15ad513156f9
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-31 14:49:56 +0000

    refactor: import the function

--- app/api/volunteer/route.ts
+++ app/api/volunteer/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    await dbConnect;
+    await dbConnect();
 
     const data = await req.json();
 
