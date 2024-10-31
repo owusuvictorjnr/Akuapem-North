3a7ec48a7eb34753b9fcd0548f86d2c01eef7137
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-31 15:50:41 +0000

    refactored home page news component

--- components/News.tsx
+++ components/News.tsx
@@ -1,47 +1,34 @@
-"use client";
-
-import axios from "axios";
-import Image from "next/image";
 import { useEffect, useState } from "react";
-
-interface NewsItem {
-  id: number;
-  title: string;
-  description: string;
-  date: string;
-  imageUrl: string;
-}
+import { newsData } from "@/data/newsData";
+import Image from "next/image";
 
 export default function NewsPage() {
-  const [news, setNews] = useState<NewsItem[]>([]);
+  const [news] = useState(newsData);
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const resp = await axios.get("/api/news");
-        setNews(resp.data);
-      } catch (error) {
-        console.error("Error fetching news:", error);
-      }
-    };
-    fetchNews();
+    // Optionally, fetch news from an API endpoint if needed
   }, []);
 
   return (
     <div className="mt-36">
-      <h1 className="text-center capitalize font-bold text-2xl">news and events</h1>
-
-      <div className="">
+      <h1 className="text-center font-bold text-2xl">News and Events</h1>
+      <div className="flex flex-col items-center px-10 gap-10 md:grid grid-cols-2 mx-auto md:grid-cols-2 lg:grid-cols-4">
         {news.map((item) => (
-          <div className="" key={item.id}>
-            <h2 className="">{item.title}</h2>
-            <p className="">{item.description}</p>
-            <Image
-              src={item.imageUrl}
-              alt={item.title}
-              height={500}
-              width={500}
-            />
+          <div
+            key={item.id}
+            className="bg-gray-200 rounded w-[50vh] md:w-full shadow-md mx-auto cursor-pointer lg:hover:scale-95 duration-100 h-full lg:w-full"
+          >
+            <div className="">
+              <Image
+                src={item.imageUrl}
+                alt={item.title}
+                height={1000}
+                width={1000}
+                className="h-52"
+              />
+            </div>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
           </div>
         ))}
       </div>
