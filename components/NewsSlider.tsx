54d07ac3c0b462ac72c03bd5160ab528a751abeb
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-04 13:50:43 +0000

    slide text component

--- components/NewsSlider.tsx
+++ components/NewsSlider.tsx
@@ -4,14 +4,15 @@ import React from "react";
 
 const SlidingText = () => {
   return (
-    <div className="slider-container bg-black/10 mb-10  min-h-[15vh] overflow-hidden">
-      <div className="slider-content space-x-10 capitalize font-semibold flex justify-center mt-10 text-3xl items-center">
-        <div className="slider-text flex space-x-10 gap-28">
-          100% secure payment
+    <div className="slider-container text-[#3F51B5]">
+      <div className="slider-content">
+        <div className="slider-text">
+          Advancing Freedom, Securing Prosperity
         </div>
-        <div className="slider-text flex  gap-28">Free worldwide shipping</div>
-        <div className="slider-text flex  gap-28">24/7 customer support</div>
-        <div className="slider-text flex  gap-28">Satisfaction guaranteed</div>
+        <div className="slider-text">Committed to Economic Growth</div>
+        <div className="slider-text">Together for a Better Ghana</div>
+        <div className="slider-text">Empowering the Future Generation</div>
+        <div className="slider-text">Strengthening Education and Health</div>
       </div>
     </div>
   );
