8caf101cd9230f20e604795af0ace3d83c0896c6
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-04 12:33:47 +0000

    remove project data

--- app/(frontend)/projects/page.tsx
+++ app/(frontend)/projects/page.tsx
@@ -1,64 +1,15 @@
+"use client";
+
+import { projData } from "@/data/projectData";
 import Image from "next/image";
-import React from "react";
+import { useEffect, useState } from "react";
 
-const projImage = [
-  {
-    title: "Water Supply Initiative in Akropong",
-    description:
-      "This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong",
-    image: "/proj/proj-8.jpg",
-  },
-
-  {
-    title: "Road Construction in Abiriw",
-    description:
-      "This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw",
-    image: "/proj/proj-1.jpg",
-  },
-
-  {
-    title: "Education Initiative in Larteh",
-    description:
-      "This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh",
-    image: "/proj/proj-2.jpg",
-  },
-
-  {
-    title: "Agricultural Initiative in Adukrom",
-    description:
-      "This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom",
-    image: "/proj/proj-3.jpg",
-  },
-
-  {
-    title: "Youth Empowerment in Mamfe",
-    description:
-      "This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe",
-    image: "/proj/proj-4.jpg",
-  },
-
-  {
-    title: "Water Supply Initiative in Akropong",
-    description:
-      "This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong",
-    image: "/proj/proj-5.png",
-  },
-
-  {
-    title: "Healthcare Initiative in Amanokrom",
-    description:
-      "This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom",
-    image: "/proj/proj-6.jpg",
-  },
-
-  {
-    title: "Road Construction in Abiriw",
-    description:
-      "This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw",
-    image: "/proj/proj-7.jpg",
-  },
-];
 export default function ProjectPage() {
+  const [project] = useState(projData);
+
+  useEffect(() => {
+    // Optionally, fetch news from an API endpoint if needed
+  }, []);
   return (
     <div className="mb-10">
       <div className="">
@@ -67,9 +18,9 @@ export default function ProjectPage() {
         </h1>
       </div>
       <div className="flex flex-col lg:grid lg:grid-cols-2 gap-10 px-5">
-        {projImage.map((project, i) => (
+        {project.map((project, id) => (
           <div
-            key={i}
+            key={id}
             className="bg-gray-200 min-h-[35rem] lg:flex pb-10 flex  mx-auto container shadow-lg rounded-lg md:w-[40rem]"
           >
             <div className="space-y-3">
