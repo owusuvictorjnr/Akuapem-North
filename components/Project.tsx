686a1f305a7d263cbdd7c70ffb26d604837cf045
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-04 12:34:30 +0000

    dynamic project data

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,59 +1,20 @@
+import { projData } from "@/data/projectData";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ProjImage = [
-  {
-    description: "Water Supply Initiative in Akropong",
-    image: "/proj/proj-8.jpg",
-  },
-
-  {
-    description: "Road Construction in Abiriw",
-    image: "/proj/proj-1.jpg",
-  },
-
-  {
-    description: "Education Initiative in Larteh",
-    image: "/proj/proj-2.jpg",
-  },
-
-  {
-    description: "Agricultural Initiative in Adukrom",
-    image: "/proj/proj-3.jpg",
-  },
-
-  {
-    description: "Youth Empowerment in Mamfe",
-    image: "/proj/proj-4.jpg",
-  },
-
-  {
-    description: "Water Supply Initiative in Akropong",
-    image: "/proj/proj-5.png",
-  },
-
-  {
-    description: "Healthcare Initiative in Amanokrom",
-    image: "/proj/proj-6.jpg",
-  },
-
-  {
-    description: "Road Construction in Abiriw",
-    image: "/proj/proj-7.jpg",
-  },
-];
-
 const Project = () => {
+  const recentProjects = projData.slice(0, 4); // Show only a few items on the home page
+
   return (
     <div className="mt-36 min-h-[20rem] space-y-10">
       <div className="flex justify-center">
         <h1 className="font-bold text-2xl">Projects</h1>
       </div>
       <div className="flex flex-col items-center px-10 gap-10 md:grid grid-cols-2 mx-auto md:grid-cols-2 lg:grid-cols-4">
-        {ProjImage.map((project, i) => (
+        {recentProjects.map((project, id) => (
           <div
-            key={i}
+            key={id}
             className="bg-gray-200 rounded w-full md:w-full shadow-md mx-auto cursor-pointer lg:hover:scale-95 duration-100 h-full lg:w-full"
           >
             <div className="text-center">
@@ -66,7 +27,7 @@ const Project = () => {
                   className="h-52 w-full"
                 />
                 <div className="flex justify-center items-center mt-5 pb-5">
-                  <h2 className="py-2">{project.description}</h2>
+                  <h2 className="py-2">{project.title}</h2>
                 </div>
               </Link>
             </div>
