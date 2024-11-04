6d9ac56ed9be51cdec7e76a5921dba73b8141aab
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-04 16:06:36 +0000

    members dataset

---
+++ data/memberData.ts
@@ -0,0 +1,83 @@
+// Regional Members Data
+export const regMembersData = [
+  {
+    name: "Jeff Konadu Addo",
+    position: "Chairperson",
+    image: "/img/john_doe.jpg",
+  },
+  {
+    name: "Twum Barimah Koranteng",
+    position: "1st Vice Chairperson",
+    image: "/img/jane_smith.jpg",
+  },
+  {
+    name: "Frank Appiah",
+    position: "2nd Vice Chairperson",
+    image: "/img/kwame_boateng.jpg",
+  },
+  {
+    name: "Tony Osei-Adjei",
+    position: "Secretary",
+    image: "/img/ama_mensah.jpg",
+  },
+
+  {
+    name: "Nana Yaw Papin",
+    position: "Assistant Secretary",
+    image: "/img/ama_mensah.jpg",
+  },
+
+  {
+    name: "Bernard Kumi Larbi",
+    position: "Treasurer",
+    image: "/img/ama_mensah.jpg",
+  },
+
+  {
+    name: "Jerry Osei Poku",
+    position: "Organiser",
+    image: "/img/ama_mensah.jpg",
+  },
+
+  {
+    name: "Adawu Musah Raha",
+    position: "Youth Organiser",
+    image: "/img/ama_mensah.jpg",
+  },
+
+  {
+    name: " Mercy Amoh Darkoah",
+    position: "Women’s Organiser",
+    image: "/img/ama_mensah.jpg",
+  },
+
+  {
+    name: "Hussen Mohammed Fadilu",
+    position: "Nasara Coordinator",
+    image: "/img/ama_mensah.jpg",
+  },
+];
+
+// Constituency Members Data
+export const ConstMembersData = [
+  {
+    name: "John Doe",
+    position: "Youth Organizer",
+    image: "/img/john_doe.jpg",
+  },
+  {
+    name: "Jane Smith",
+    position: "Women's Leader",
+    image: "/img/jane_smith.jpg",
+  },
+  {
+    name: "Kwame Boateng",
+    position: "Community Organizer",
+    image: "/img/kwame_boateng.jpg",
+  },
+  {
+    name: "Ama Mensah",
+    position: "Communications Director",
+    image: "/img/ama_mensah.jpg",
+  },
+];
