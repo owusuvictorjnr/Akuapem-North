043296fc43d2a63085d57aba8dbc42026fd0f567
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-04 12:34:43 +0000

    project data

---
+++ data/projectData.ts
@@ -0,0 +1,65 @@
+export const projData = [
+  {
+    id: 1,
+    title: "Water Supply Initiative in Akropong",
+    description:
+      "This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong",
+    image: "/proj/proj-8.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Road Construction in Abiriw",
+    description:
+      "This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw",
+    image: "/proj/proj-1.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Education Initiative in Larteh",
+    description:
+      "This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh This is an education initiative in Larteh",
+    image: "/proj/proj-2.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Agricultural Initiative in Adukrom",
+    description:
+      "This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom This is an agricultural initiative in Adukrom",
+    image: "/proj/proj-3.jpg",
+  },
+
+  {
+    id: 5,
+    title: "Youth Empowerment in Mamfe",
+    description:
+      "This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe This is a youth empowerment project in Mamfe",
+    image: "/proj/proj-4.jpg",
+  },
+
+  {
+    id: 6,
+    title: "Water Supply Initiative in Akropong",
+    description:
+      "This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong This is a water supply initiative in Akropong",
+    image: "/proj/proj-5.png",
+  },
+
+  {
+    id: 7,
+    title: "Healthcare Initiative in Amanokrom",
+    description:
+      "This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom This is a healthcare initiative in Amanokrom",
+    image: "/proj/proj-6.jpg",
+  },
+
+  {
+    id: 8,
+    title: "Road Construction in Abiriw",
+    description:
+      "This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw This is a road construction project in Abiriw",
+    image: "/proj/proj-7.jpg",
+  },
+];
