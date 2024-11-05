7b4469d700bb9fce2a2ae05e0f4dd8a19f651f19
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-05 06:36:17 +0000

    refactor: styles

--- app/(frontend)/projects/page.tsx
+++ app/(frontend)/projects/page.tsx
@@ -13,7 +13,7 @@ export default function ProjectPage() {
   return (
     <div className="mb-10">
       <div className="">
-        <h1 className="text-center text-2xl font-bold uppercase mb-4 bg-gray-200 py-2">
+        <h1 className="text-center md:text-2xl text-lg text-blue-500 font-bold uppercase mb-4 bg-gray-200 py-2">
           Projects
         </h1>
       </div>
@@ -23,7 +23,7 @@ export default function ProjectPage() {
             key={id}
             className="bg-gray-200 min-h-[35rem] lg:flex pb-10 flex  mx-auto container shadow-lg rounded-lg md:w-[40rem]"
           >
-            <div className="space-y-3">
+            <div className="space-y-3  mx-auto w-full">
               <Image
                 src={project.image}
                 alt={project.title}
@@ -32,10 +32,10 @@ export default function ProjectPage() {
                 className="w-full h-[30vh] md:h-[45vh]"
               />
               <div className="space-y-8">
-                <h1 className="text-center text-lg font-bold text-black/50 md:text-2xl">
+                <h1 className="text-center text-lg font-bold text-blue-500 md:text-2xl">
                   {project.title}
                 </h1>
-                <p className="px-5">{project.description}</p>
+                <p className="px-5 leading-relaxed text-[#565358]">{project.description}</p>
               </div>
             </div>
           </div>
