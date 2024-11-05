cee131f4a1d208d8347a4b3145b91a43bc0f5e6d
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-05 06:41:16 +0000

    refactor: description

--- data/newsData.ts
+++ data/newsData.ts
@@ -5,22 +5,23 @@ export const newsData = [
     id: 1,
     title: "New Constituency Hospital Launched",
     description:
-      "The Akuapem North constituency launches a new hospital to improve healthcare services.",
+      "The Akuapem North constituency launches a new hospital to improve healthcare services. The Akuapem North constituency launches a new hospital to improve healthcare services. The Akuapem North constituency launches a new hospital to improve healthcare services. The Akuapem North constituency launches a new hospital to improve healthcare services. The Akuapem North constituency launches a new hospital to improve healthcare services.",
     date: "2024-10-20",
     imageUrl: "/proj/sami-award.jpeg",
   },
   {
     id: 2,
     title: "Town Hall Meeting with Party Officials",
-    description: "Engage with key officials at our upcoming town hall meeting.",
+    description:
+      "Engage with key officials at our upcoming town hall meeting. Engage with key officials at our upcoming town hall meeting. Engage with key officials at our upcoming town hall meeting. Engage with key officials at our upcoming town hall meeting. Engage with key officials at our upcoming town hall meeting. Engage with key officials at our upcoming town hall meeting. Engage with key officials at our upcoming town hall meeting. Engage with key officials at our upcoming town hall meeting.",
     date: "2024-10-15",
-    imageUrl: "/img/bawu.png",
+    imageUrl: "/img/vote.png",
   },
   {
     id: 3,
     title: "Education Fundraiser for Youth Development",
     description:
-      "Join us in raising funds for the constituency's youth educational development.",
+      "Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development.",
     date: "2024-10-10",
     imageUrl: "/img/camp-1.jpg",
   },
@@ -29,7 +30,7 @@ export const newsData = [
     id: 4,
     title: "Education Fundraiser for Youth Development",
     description:
-      "Join us in raising funds for the constituency's youth educational development.",
+      "Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development. Join us in raising funds for the constituency's youth educational development.",
     date: "2024-10-10",
     imageUrl: "/img/camp-1.jpg",
   },
