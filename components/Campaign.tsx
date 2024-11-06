2619c962715c5ad02e0b2587e70f21450fc59930
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-06 08:32:42 +0000

    refactor: message paragraph

--- components/Campaign.tsx
+++ components/Campaign.tsx
@@ -13,20 +13,18 @@ const Campaigns = () => {
       </div>
 
       <div className="space-y-3 text-center">
-        <h1 className="capitalize font-bold text-3xl">message</h1>
+        <h1 className="capitalize font-bold text-blue-700 text-3xl">message</h1>
 
-        <p className="font-semibold text-xl">Empowering Communities, Building the Future</p>
+        {/* <p className="font-semibold text-xl text-blue-400">Empowering Communities, Building the Future</p> */}
       </div>
-      <p className="text-center mx-auto container text-gray-400 lg:px-[20rem]">
-        Our mission is to create a prosperous, united Akuapem North where every
-        individual has the opportunity to thrive. Through transparent
-        leadership, we aim to promote development in education, healthcare,
-        infrastructure, and economic empowerment. Together, we will ensure that
-        no one is left behind in our pursuit of progress. With your support, we
-        will continue to build a strong foundation for future generations, drive
-        meaningful change, and uphold the values of integrity and service that
-        the New Patriotic Party stands for. Let&#39;s rise together for a
-        brighter tomorrow.
+      <p className="text-justify mx-auto container text-[#565358] lg:px-[20rem]">
+        Through transparent leadership, we aim to promote development in
+        education, healthcare, infrastructure, and economic empowerment.
+        Together, we will ensure that no one is left behind in our pursuit of
+        progress. With your support, we will continue to build a strong
+        foundation for future generations, drive meaningful change, and uphold
+        the values of integrity and service that the New Patriotic Party stands
+        for. Let&#39;s rise together for a brighter tomorrow.
       </p>
     </div>
   );
