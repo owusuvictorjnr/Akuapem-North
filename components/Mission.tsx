c9307efebd47722c49f6875960f63cf910fb52c7
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-06 08:33:47 +0000

    refactor:  link to mission page

--- components/Mission.tsx
+++ components/Mission.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { TfiAnchor } from "react-icons/tfi";
 
@@ -10,20 +11,21 @@ const Mission = () => {
         className="bg-red-500 rounded-full py-2 px-2
       hover:bg-blue-950"
       />
-      <h1 className="capitalize font-bold text-3xl">
+      <h1 className="capitalize font-bold text-blue-700 text-3xl">
         our mission
       </h1>
-      <p className="text-center mx-auto container text-gray-400 lg:px-[20rem]">
-        To build a united, prosperous, and progressive Akuapem North, where
-        every citizen is empowered to reach their full potential. Through
-        transparent governance, inclusive policies, and community-driven
-        initiatives, we will strengthen our educational systems, enhance
-        healthcare, improve infrastructure, and foster economic opportunities.
-        Together, we will lay the foundation for sustainable development,
-        ensuring that the voice of every citizen is heard and their needs are
-        met. Our mission is to serve with integrity, promote equality, and drive
-        the constituency toward a brighter and more prosperous future.
-      </p>
+      <div className="">
+        <p className="text-justify mx-auto line-clamp-2 container text-[#565358] lg:px-[20rem]">
+          To achieve this vision, we are dedicated to implementing strategic
+          programs and initiatives that address the unique needs of Akuapem
+          North. Our mission is to empower the youth through quality education,
+          support local businesses for economic growth, and provide essential
+          services that enhance the well-being of every individual in
+        </p>
+        <Link href="/about/mission-vision" className="text-blue-700 hover:text-red-500">
+          read more
+        </Link>
+      </div>
     </div>
   );
 };
